ed7d21fd65b589a9f58e6b55b7103e44718bbe19
Kyziava <v_anyak@ukr.net>
2024-04-26 08:03:44 +0000

    gallerie scrol by fixed amount nav center

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
-function scrollGallery(direction) {
-    const gallery = document.getElementById('imageGallery');
-    gallery.scrollBy({
-      left: direction * gallery.offsetWidth, // Scroll by one screen width
-      behavior: 'smooth' // Smooth scrolling
-    });
-  }
+
+function scrollRight() {
+  const scrollAmount = 600; 
+  const gallery = document.getElementById('imageGallery');
+  gallery.scrollBy({
+    left: -scrollAmount, // Scroll left by 200
+    behavior: 'smooth' // Smooth scrolling
+  });
+}
+
+function scrollRight2() {
+  const scrollAmount = 600; 
+  const gallery = document.getElementById('imageGallery');
+  gallery.scrollBy({
+    left: scrollAmount, // Scroll right by 200
+    behavior: 'smooth' // Smooth scrolling
+  });
+}
